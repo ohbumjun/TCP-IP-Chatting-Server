ba8334ebbb29161deb74af9173439eaa1d07b68c
ohbumjun <beomjun0638@hanmail.net>
2023-01-08 23:03:51 +0900

    feat(Engine) Serialize By Not Making Instance, But Directory Accessing Buffer Data

--- DummyClient/ClientPacketHandler.cpp
+++ DummyClient/ClientPacketHandler.cpp
@@ -31,7 +31,7 @@ void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 #pragma pack(1)
 struct PKT_S_TEST
 {
-	// 패킷 설계 TEMP
+	// 패킷 설계 TEMP 가변 길이 구조체 
 	struct BuffListItem
 	{
 		uint64 buffId;
@@ -50,8 +50,16 @@ struct PKT_S_TEST
 	// [PKT_S_TEST][BufferData, BufferData, BufferData]
 	//			중에서 BufferData 가 시작하는 위치
 	uint16 buffsOffset;
+
 	// 가변데이터 총 개수 
 	uint16 buffsCount;
+
+	// 아래와 같이 내부에 데이터를 받아들일 필요 없이
+	// 1) 문자열 (ex. name)
+	// 2) 그냥 바이트 배열 (ex. 길드 이미지)
+	// 3) 일반 리스트
+	// vector<BuffListItem> buffs;
+	// wstring name;
 
 	bool Validate()
 	{
@@ -59,6 +67,10 @@ struct PKT_S_TEST
 
 		// PKT_S_TEST 만큼의 Size 추가
 		size += sizeof(PKT_S_TEST);
+
+		if (size > packetSize)
+			return false;
+
 		// 가변 데이터 크기까지 추가
 		size += buffsCount * sizeof(BuffListItem);
 
@@ -72,13 +84,16 @@ struct PKT_S_TEST
 		return true;
 	}
 
+	// 가변 데이터들의 배열 형태 
+	using BuffsList = PacketList<PKT_S_TEST::BuffListItem>;
 
-	// 가변 데이터가 추가된다고 해보가
-	// 1) 문자열 (ex. name)
-	// 2) 그냥 바이트 배열 (ex. 길드 이미지)
-	// 3) 일반 리스트
-	// vector<BuffListItem> buffs;
-	// wstring name;
+	BuffsList GetBuffsList()
+	{
+		// 시작 주소 + offset == 가변 데이터 있는 시작 위치
+		BYTE* data = reinterpret_cast<BYTE*>(this);
+		data += buffsOffset;
+		return BuffsList(reinterpret_cast<PKT_S_TEST::BuffListItem*>(data), buffsCount);
+	}
 };
 
 #pragma pack()
@@ -90,90 +105,54 @@ void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 	if (len < sizeof(PKT_S_TEST))
 		return;
 
-	// PacketHeader header;
-	// br >> header;
-	// uint64 id;
-	// uint32 hp;
-	// uint16 attack;
-	// br >> id >> hp >> attack;
-	// cout << "Id, Hp, Att : " << id << "," << hp << "." << attack << endl;
+	/*
+	1번째 방법 : 각 원소들을 하나하나 받는 방식
+	PacketHeader header;
+	br >> header;
+	uint64 id;
+	uint32 hp;
+	uint16 attack;
+	br >> id >> hp >> attack;
+	cout << "Id, Hp, Att : " << id << "," << hp << "." << attack << endl;
+	*/
+	
+	/*
+	2번째 : 직렬화 1st => 임시 객체 만들어서 데이터 받기 
 	PKT_S_TEST pkt;
 	br >> pkt;
+	*/
 
-	if (pkt.Validate() == false)
+	/*
+	3번째 : 직렬화 2nd => 임시 객체 안만들고 buffer 에서 바로 받아오기 
+	*/
+	PKT_S_TEST* pkt = reinterpret_cast<PKT_S_TEST*>(buffer);
+
+	if (pkt->Validate() == false)
 		return;
 
-	/*
-	(PacketSession 활용)
-	PacketHeader header = *((PacketHeader*)&buffer[0]);
-	cout << "Packet Id, Size : " << header.id << "," << header.size << endl;
-
-	char recvBuffer[4096];
-	::memcpy(recvBuffer, &buffer[4], header.size - sizeof(PacketHeader));
-
-	char recvBuffer[4096];
-
-	// 중간에 끼어넣은 id, hp, attack 고려하여 size 계산
-	br.Read(recvBuffer, header.size - sizeof(PacketHeader) - 8 - 4 - 2);
-
-	cout << recvBuffer << endl;
-	*/
-
-	/*
-	(제일 기본적인 형태)
-	this_thread::sleep_for(1s);
-
-	// Server 측에서 에코서버 방식으로 데이터 다시 보내주면
-	// 다시 또 보내기
-	SendBufferRef sendBuffer = GSendBufferManager->Open(4096);
-	::memcpy(sendBuffer->Buffer(), sendData, sizeof(sendData));
-
-	// Echo Server 기능
-	// sendBuffer->CopyData(buffer, len);
-	sendBuffer->Close(sizeof(sendData));
-
-	Send(sendBuffer);
-	*/
-
 	// SeverPacketHandler 에서 보낸 가변 데이터 길이 받아들이기 
-	/*
-	(기존 방식)
-	vector<BufferLs> buffs;
-	uint16 buffCount;
-	br >> buffCount;
+	// 아래와 같이 객체를 만들어서 가변 데이터를 받아들이는 것이 아니라,
+	// 마찬가지로 포인터 형태로 메모리를 다른 방식으로 바라볼 것이다 (형변환)
+	// vector<PKT_S_TEST::BuffListItem> buffs;
+	// buffs.resize(pkt->buffsCount);
+	PKT_S_TEST::BuffsList buffs = pkt->GetBuffsList();
+	
+	cout << "BufCount : " << pkt->buffsCount << endl;
 
-	buffs.resize(buffCount);
-	for (int32 i = 0; i < buffCount; i++)
-	{
-		// BufferReader 클래스 내에 있는 메모리
-		// 로부터 데이터 가져오기 
-		br >> buffs[i].buffId >> buffs[i].remainTime;
-	}
-
-	cout << "BufCount : " << buffCount << endl;
-
-	for (int32 i = 0; i < buffCount; i++)
-	{
-		cout << "BufInfo : " << buffs[i].buffId << " " << buffs[i].remainTime << endl;
-	}
-	*/
-
-	vector<PKT_S_TEST::BuffListItem> buffs;
-	buffs.resize(pkt.buffsCount);
-
-	for (int32 i = 0; i < pkt.buffsCount; ++i)
-	{
-		// BufferReader 클래스 내에 있는 메모리
-		// 로부터 데이터 가져오기 
-		br >> buffs[i];
-	}
-
-	cout << "BufCount : " << pkt.buffsCount << endl;
-
-	for (int32 i = 0; i < pkt.buffsCount; i++)
+	for (int32 i = 0; i < pkt->buffsCount; i++)
 	{
 		cout << "BufInfo : " << buffs[i].buffId << " " << buffs[i].remainTime << endl;
 	};
+
+	for (auto it = buffs.begin(); it != buffs.end(); ++it)
+	{
+		cout << "BufInfo : " << it->buffId << " " << it->remainTime << endl;
+	}
+
+	for (auto& buff : buffs)
+	{
+		cout << "BufInfo : " << buff.buffId << " " << buff.remainTime << endl;
+	}
 
 	wstring name;
 	uint16 nameLen;
